bf758f555e722242d0be26e1a7398145f3946c23
amiller5233@gmail.com <amiller5233@gmail.com>
2023-07-15 15:24:48 -0700

    create xcode project

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Adam Miller on 7/15/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
