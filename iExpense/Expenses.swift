e11581824b8abdab0e62f71ffb3a032809549b41
amiller5233@gmail.com <amiller5233@gmail.com>
2023-07-16 12:18:43 -0700

    Save expenses to UserDefaults

--- iExpense/Expenses.swift
+++ iExpense/Expenses.swift
@@ -8,5 +8,22 @@
 import Foundation
 
 class Expenses: ObservableObject {
-    @Published var items = [ExpenseItem]()
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "iExpense-Items")
+            }
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "iExpense-Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        
+        items = []
+    }
 }
